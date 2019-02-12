b86a80cd4ab3b8036164943a967ed7d8dd52eb84
Michael Valdes <pandamayc_20@hotmail.com>
2019-02-11 19:03:31 -0600

    adding new features

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,15 @@
 $(document).ready(function()
 {
-	//particlesJS();
-	$('.navbar-toggle').click(function(){
-        $('.navbar-toggle').toggleClass('navbar-on');
-        $('nav').fadeToggle();
-        $('nav').removeClass('nav-hide');
-        $('#close_menu').toggleClass('show_close_menu');
-    });
+    //particlesJS();
 });
+
+
+var bTwitter = document.getElementById("twitter");
+var bInstagram = document.getElementById("instagram");
+var bLinkedIn = document.getElementById("linkedin");
+var bGitHub = document.getElementById("github");
+
+bTwitter.addEventListener('click', function(){ window.open('https://twitter.com/maycman/'); });
+bInstagram.addEventListener('click', function(){ window.open('https://www.instagram.com/maycman13/'); });
+bLinkedIn.addEventListener('click', function(){ window.open('https://www.linkedin.com/in/maycman/'); });
+bGitHub.addEventListener('click', function(){ window.open('https://github.com/maycman/'); });
